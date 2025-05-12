6e8e5f575c00a6907ae6474d436acdd3ab0a25bc
luis <mail>
2025-05-11 22:25:16 -0500

    req 9 y 10

--- LuisC/DetallesJuegos/req9.js
+++ LuisC/DetallesJuegos/req9.js
@@ -1,32 +1,51 @@
 const juegos = [
-    {
-      id: 1,
-      nombre: "Zelda: Breath of the Wild",
-      descripcion: "Un juego de aventuras en un mundo abierto impresionante.",
-      imagen: "https://i.imgur.com/O3QfKxD.jpeg",
-      trailer: "https://www.youtube.com/embed/zw47_q9wbBE",
-      rating: 5,
-      resenas: [
-        "Una obra maestra.",
-        "Increíble mundo abierto.",
-        "10/10 en todos los aspectos."
-      ]
-    },
-    {
-      id: 2,
-      nombre: "God of War Ragnarok",
-      descripcion: "Kratos y Atreus enfrentan su destino en la mitología nórdica.",
-      imagen: "https://i.imgur.com/fptQtxS.jpeg",
-      trailer: "https://www.youtube.com/embed/EE-4GvjKcfs",
-      rating: 4,
-      resenas: [
-        "Gran historia y combate.",
-        "Mejor que el anterior.",
-        "Gráficos espectaculares."
-      ]
-    }
-  ];
-  
+  {
+    id: 1,
+    nombre: "Zelda",
+    descripcion: "Un juego de aventuras en un mundo abierto impresionante.",
+    imagen: "https://i.imgur.com/O3QfKxD.jpeg",
+    trailer: "https://www.youtube.com/embed/zw47_q9wbBE",
+    rating: 5,
+    resenas: ["Una obra maestra.", "Increíble mundo abierto.", "10/10 en todos los aspectos."]
+  },
+  {
+    id: 2,
+    nombre: "God of War",
+    descripcion: "Kratos y Atreus enfrentan su destino en la mitología nórdica.",
+    imagen: "https://i.imgur.com/fptQtxS.jpeg",
+    trailer: "https://www.youtube.com/embed/EE-4GvjKcfs",
+    rating: 4,
+    resenas: ["Gran historia y combate.", "Mejor que el anterior.", "Gráficos espectaculares."]
+  },
+  {
+    id: 3,
+    nombre: "Hollow Knight",
+    descripcion: "Una aventura de acción en 2D con exploración y desafíos.",
+    imagen: "https://i.imgur.com/vzGmP4X.jpeg",
+    trailer: "https://www.youtube.com/embed/UAO2urG23S4",
+    rating: 5,
+    resenas: ["Una joya indie.", "Muy desafiante.", "Arte hermoso."]
+  },
+  {
+    id: 4,
+    nombre: "Elden Ring",
+    descripcion: "Un vasto mundo de fantasía lleno de secretos y combates difíciles.",
+    imagen: "https://i.imgur.com/Sz7j4Ql.jpeg",
+    trailer: "https://www.youtube.com/embed/E3Huy2cdih0",
+    rating: 5,
+    resenas: ["Obra maestra de FromSoftware.", "Difícil pero justo.", "Mundo inmersivo."]
+  },
+  {
+    id: 5,
+    nombre: "Celeste",
+    descripcion: "Una historia conmovedora y plataformas de precisión.",
+    imagen: "https://i.imgur.com/WVG3DAE.jpeg",
+    trailer: "https://www.youtube.com/embed/iofYDsPqAAA",
+    rating: 4,
+    resenas: ["Inspirador.", "Difícil pero gratificante.", "Excelente música."]
+  }
+];
+
   function getParametroURL(nombre) {
     const params = new URLSearchParams(window.location.search);
     return params.get(nombre);
