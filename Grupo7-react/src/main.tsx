164b635bb0f660fba2b3daa43b7e7bbb033f8e06
luis <mail>
2025-05-20 17:05:32 -0500

    react parte 2

--- Grupo7-react/src/main.tsx
+++ Grupo7-react/src/main.tsx
@@ -7,9 +7,12 @@ import NotificacionCompra from './Pages/NotificacionCompra'
 import Detalle from './Pages/Detalle'
 import Reseña from './Pages/Reseña'
 import AdminJuegos from './Pages/AdminJuegos'
+import AgregarJuego from './Pages/AgregarJuego'
+import EditarJuego from './Pages/EditarJuego'
+import EliminarJuego from './Pages/EliminarJuego'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <AdminJuegos/>
+   <PaginaPrincipal/>
    
   </StrictMode>,
 )
