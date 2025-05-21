8975af29a6eb3ab144ef5b938c6ce71f93524165
AmdleGuillen <hoxtocabra@gmail.com>
2025-05-21 09:08:59 -0500

    Main con bootstrap

--- Grupo7-react/src/main.tsx
+++ Grupo7-react/src/main.tsx
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import ConfirmarOrden from './Pages/Confirmarorden';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
